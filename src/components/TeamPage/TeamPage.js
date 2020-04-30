7246c87a0bf877547b7b11d26a218d2a371f760e
Mark Spannbauer <msmarkieer@gmail.com>
2020-04-29 19:08:21 -0500

    Added a tabbed section to the team page

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -6,9 +6,13 @@ import './TeamPage.css'
 
 
 // Ant Design
-import { Row, Col } from 'antd';
+import { Row, Col, Tabs } from 'antd';
+const { TabPane } = Tabs;
 
 class PlayerPage extends Component {
+    state = {
+        isLeader: false
+    }
 
     componentDidMount() {
         console.log('ID:', this.props.match.params.id);
@@ -19,30 +23,45 @@ class PlayerPage extends Component {
     render() {
         return (
             <div>
-                <Row>
-                    <Col span={1}></Col>
-                    <Col span={3} className="player-info">
-                        {this.props.team[0] ?
-                            <>
-                                <h1 id="welcome">
-                                    {this.props.team[0].name}
+                {this.props.team[0] ?
+                    <>
+                        <Row>
+                            <Col span={24}>
+                                <h1 id="welcome" className="team-name">
+                                {this.props.team[0].name}
                                 </h1>
-                                <p>Team ID: {this.props.team[0].id}</p>
-                            </> :
-                            <p>User not found</p>
-                        }
-                    </Col>
-                    <Col span={19}>
-                        {/* Soon to be graphs */}
-                    </Col>
-                    <Col span={1}></Col>
-                </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={2}></Col>
+                            <Col span={20}>
+                                <Tabs defaultActiveKey="1" type="card" size="large">
+                                    <TabPane tab="Players" key="1">
+                                        PLAYER TABLE
+                                    </TabPane>
+                                    <TabPane tab="Stats" key="2">
+                                        STATS
+                                    </TabPane>
+                                    {this.state.isLeader &&
+                                        <TabPane tab="Manage" key="3">
+                                            MANAGEMENT TOOLS
+                                        </TabPane>
+                                    }
+                                    
+                                </Tabs>
+                            </Col>
+                            <Col span={2}></Col>
+                        </Row>
+                    </> :
+                    <p>User not found</p>
+                }
+                
                 
             </div>
         );
     }
 }
 
-const mapStateToProps = ({ team }) => ({ team });
+const mapStateToProps = ({ team, user }) => ({ team, user });
 
 export default connect(mapStateToProps)(PlayerPage);
