b80b55514b4fb2960c063155b44cc69055d3e29a
Mark Spannbauer <msmarkieer@gmail.com>
2020-05-04 14:13:34 -0500

    router now parses kills and deaths per player

--- server/routes/log.router.js
+++ server/routes/log.router.js
@@ -45,13 +45,38 @@ router.post('/', (req, res) => {
                         for (const key in log.players) {
                             if (log.players.hasOwnProperty(key)) {
                                 const player = log.players[key];
+                                const playerKills = log.classkills[key]
+                                const playerDeaths = log.classdeaths[key]
                                 
                                 let queryText = `INSERT INTO "log_stats" 
                                                     ("log_id", "steamid3", "team", "assists", "suicides", "kapd", "kpd", 
                                                     "damage", "damage_taken", "dapm", "ubers", "drops", "backstabs", "headshots")
-                                                    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`;
+                                                    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING "id";`;
                                 pool.query(queryText,[log_id, key, player.team, player.assists, player.suicides, player.kapd, player.kpd,
                                                         player.dmg, player.dt, player.dapm, player.ubers, player.drops, player.backstabs, player.headshots_hit]).then(result => {
+
+                                    let log_stat_id = result.rows[0].id
+                                    
+                                    let queryText = `INSERT INTO "kills" ("log_stat_id", "Scout", "Soldier", "Pyro", "Demo", "Heavy", "Engineer", "Medic", "Sniper", "Spy") 
+                                                        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
+                                    pool.query(queryText, [log_stat_id, playerKills.scout || 0, playerKills.soldier || 0,
+                                        playerKills.pyro || 0, playerKills.demoman || 0, playerKills.heavyweapons || 0,
+                                        playerKills.engineer || 0, playerKills.medic || 0, playerKills.sniper || 0, playerKills.spy || 0,]).then(result => {
+
+                                            let queryText = `INSERT INTO "deaths" ("log_stat_id", "Scout", "Soldier", "Pyro", "Demo", "Heavy", "Engineer", "Medic", "Sniper", "Spy") 
+                                                        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
+                                            pool.query(queryText, [log_stat_id, playerDeaths.scout || 0, playerDeaths.soldier || 0,
+                                                playerDeaths.pyro || 0, playerDeaths.demoman || 0, playerDeaths.heavyweapons || 0,
+                                                playerDeaths.engineer || 0, playerDeaths.medic || 0, playerDeaths.sniper || 0, playerDeaths.spy || 0,]).then(result => {
+
+                                                })
+                                                .catch(error => {
+                                                    console.log('error posting into "deaths"', error);
+                                                });
+                                    })
+                                    .catch(error => {
+                                        console.log('error posting into "kills"', error);
+                                    });
                                 })
                                 .catch(error => {
                                     console.log('error posting into "log_stats"', error);
