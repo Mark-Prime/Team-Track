0f20e3dc1611d3ea85cb3f8869930664d3a38eeb
Mark Spannbauer <msmarkieer@gmail.com>
2020-05-31 16:35:31 -0500

    added a donator tag to display on donators profiles

--- src/components/PlayerPage/PlayerPage.js
+++ src/components/PlayerPage/PlayerPage.js
@@ -67,143 +67,214 @@ class PlayerPage extends Component {
 
     render() {
         return (
-            <div>
-                {this.props.player[0] ?
-                    <div className="flex-container">
-                        <div className="profile-info">
-                            <Avatar shape="square" size={128} src={this.props.player[0].avatar} />
-                            <h1 id="welcome">
+          <div>
+            {this.props.player[0] ? (
+              <div className="flex-container">
+                <div className="profile-info">
+                  <Avatar
+                    shape="square"
+                    size={128}
+                    src={this.props.player[0].avatar}
+                  />
+                  <h1 id="welcome">
+                    <Space>
+                      {this.props.player[0].displayname}
+                      {this.props.player[0].verified && (
+                        <Tooltip title="Verified!">
+                          <CheckCircleTwoTone />
+                        </Tooltip>
+                      )}
+                    </Space>
+                  </h1>
+                  {this.props.stats.leader_mode && (
+                    <Tag
+                      color={"orange"}
+                      key={`LEADER`}
+                      style={{ margin: "5px" }}
+                    >
+                      TEAM LEADER
+                    </Tag>
+                  )}
+                  {this.props.stats.main_mode && (
+                    <Tag color={"blue"} key={`MAIN`} style={{ margin: "5px" }}>
+                      OFTEN MAINS
+                    </Tag>
+                  )}
+                  {this.props.player[0].donator && (
+                    <Tag
+                      color={"green"}
+                      key={`DONATOR`}
+                      style={{ margin: "5px" }}
+                    >
+                      DONATOR
+                    </Tag>
+                  )}
+                  {this.props.log[0] && this.logTags(this.props.log.length)}
 
-                                <Space>
-                                    {this.props.player[0].displayname}
-                                    {this.props.player[0].verified &&
-                                        <Tooltip title="Verified!">
-                                            <CheckCircleTwoTone />
-                                        </Tooltip>}
-                                </Space>
-                            </h1>
-                            {this.props.stats.leader_mode &&
-                                <Tag color={"orange"} key={`LEADER`} style={{ margin: "5px" }}>
-                                    TEAM LEADER
-                            </Tag>
-                            }
-                            {this.props.stats.main_mode &&
-                                <Tag color={"blue"} key={`MAIN`} style={{ margin: "5px" }}>
-                                    OFTEN MAINS
-                            </Tag>
-                            }
-                            {this.props.log[0] && this.logTags(this.props.log.length)}
+                  <p>Player ID: {this.props.player[0].id}</p>
 
-                            <p>Player ID: {this.props.player[0].id}</p>
-
-                            <Dropdown className="dropdown-component" overlay={<Menu>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://steamcommunity.com/profiles/${this.props.player[0].id}`}>
-                                        Steam
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://rgl.gg//public/PlayerProfile.aspx?p=${this.props.player[0].id}`}>
-                                        RGL.gg
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://www.ugcleague.com/players_page.cfm?player_id=${this.props.player[0].id}`}>
-                                        UGC
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`http://etf2l.org/search/${this.props.player[0].id}`}>
-                                        ETF2L
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://ozfortress.com/users?q=${this.props.player[0].id}`}>
-                                        OZFortress
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://logs.tf/profile/${this.props.player[0].id}`}>
-                                        Logs.tf
-                                </a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a target="_blank" rel="noopener noreferrer" href={`https://demos.tf/profiles/${this.props.player[0].id}`}>
-                                        Demos.tf
-                                </a>
-                                </Menu.Item>
-                            </Menu>}>
-                                <h4 className="dropdown">
-                                    External Links <DownOutlined />
-                                </h4>
-                            </Dropdown>
-                        </div>
-                        <div className="player-tables">
-                            <Tabs defaultActiveKey="1" type="card" size="large">
-                                <TabPane tab="Teams" key="1">
-                                    {this.props.player[0] &&
-                                        <Table columns={[
-                                            {
-                                                title: 'Name',
-                                                dataIndex: 'name',
-                                                key: 'name',
-                                                render: (text, record) => <>
-                                                    {record.active ?
-                                                        <a href={`/#/team/${record.team_id}`}>{text}</a> :
-                                                        <span className="strike">
-                                                            <a href={`/#/team/${record.team_id}`}>{text}</a>
-                                                        </span>
-                                                    }
-
-                                                </>
-                                            },
-                                            {
-                                                title: 'Gamemode',
-                                                dataIndex: 'title',
-                                                key: 'title'
-                                            },
-                                            {
-                                                title: 'Class',
-                                                dataIndex: 'class_name',
-                                                key: 'class_name'
-                                            },
-                                            {
-                                                title: '',
-                                                key: 'is_leader',
-                                                dataIndex: 'is_leader',
-                                                render: (leader, record) => (
-                                                    <span>
-                                                        {leader &&
-                                                            <Tag color={"orange"} key={`${record.user_id}_LEADER`}>
-                                                                LEADER
-                                                </Tag>
-                                                        }
-                                                        {record.main ?
-                                                            <Tag color={"blue"} key={`${record.user_id}_MAIN`}>
-                                                                MAIN
-                                                </Tag> :
-                                                            <Tag color={"cyan"} key={`${record.user_id}_SUB`}>
-                                                                SUB
-                                                </Tag>
-                                                        }
-                                                    </span>
-                                                ),
-                                            }
-                                        ]} dataSource={this.props.team} />
-                                    }
-                                </TabPane>
-                                <TabPane tab="Stats" key="2">
-                                    {this.props.log[0] ?
-                                        <PlayerStats /> :
-                                        <h2>This Player has no Game Data</h2>}
-                                </TabPane>
-                            </Tabs>
-                        </div>
-                    </div> :
-
-                            <p>User not found</p>
-                        }
-            </div>
+                  <Dropdown
+                    className="dropdown-component"
+                    overlay={
+                      <Menu>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://steamcommunity.com/profiles/${this.props.player[0].id}`}
+                          >
+                            Steam
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://rgl.gg//public/PlayerProfile.aspx?p=${this.props.player[0].id}`}
+                          >
+                            RGL.gg
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://www.ugcleague.com/players_page.cfm?player_id=${this.props.player[0].id}`}
+                          >
+                            UGC
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`http://etf2l.org/search/${this.props.player[0].id}`}
+                          >
+                            ETF2L
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://ozfortress.com/users?q=${this.props.player[0].id}`}
+                          >
+                            OZFortress
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://logs.tf/profile/${this.props.player[0].id}`}
+                          >
+                            Logs.tf
+                          </a>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={`https://demos.tf/profiles/${this.props.player[0].id}`}
+                          >
+                            Demos.tf
+                          </a>
+                        </Menu.Item>
+                      </Menu>
+                    }
+                  >
+                    <h4 className="dropdown">
+                      External Links <DownOutlined />
+                    </h4>
+                  </Dropdown>
+                </div>
+                <div className="player-tables">
+                  <Tabs defaultActiveKey="1" type="card" size="large">
+                    <TabPane tab="Teams" key="1">
+                      {this.props.player[0] && (
+                        <Table
+                          columns={[
+                            {
+                              title: "Name",
+                              dataIndex: "name",
+                              key: "name",
+                              render: (text, record) => (
+                                <>
+                                  {record.active ? (
+                                    <a href={`/#/team/${record.team_id}`}>
+                                      {text}
+                                    </a>
+                                  ) : (
+                                    <span className="strike">
+                                      <a href={`/#/team/${record.team_id}`}>
+                                        {text}
+                                      </a>
+                                    </span>
+                                  )}
+                                </>
+                              ),
+                            },
+                            {
+                              title: "Gamemode",
+                              dataIndex: "title",
+                              key: "title",
+                            },
+                            {
+                              title: "Class",
+                              dataIndex: "class_name",
+                              key: "class_name",
+                            },
+                            {
+                              title: "",
+                              key: "is_leader",
+                              dataIndex: "is_leader",
+                              render: (leader, record) => (
+                                <span>
+                                  {leader && (
+                                    <Tag
+                                      color={"orange"}
+                                      key={`${record.user_id}_LEADER`}
+                                    >
+                                      LEADER
+                                    </Tag>
+                                  )}
+                                  {record.main ? (
+                                    <Tag
+                                      color={"blue"}
+                                      key={`${record.user_id}_MAIN`}
+                                    >
+                                      MAIN
+                                    </Tag>
+                                  ) : (
+                                    <Tag
+                                      color={"cyan"}
+                                      key={`${record.user_id}_SUB`}
+                                    >
+                                      SUB
+                                    </Tag>
+                                  )}
+                                </span>
+                              ),
+                            },
+                          ]}
+                          dataSource={this.props.team}
+                        />
+                      )}
+                    </TabPane>
+                    <TabPane tab="Stats" key="2">
+                      {this.props.log[0] ? (
+                        <PlayerStats />
+                      ) : (
+                        <h2>This Player has no Game Data</h2>
+                      )}
+                    </TabPane>
+                  </Tabs>
+                </div>
+              </div>
+            ) : (
+              <p>User not found</p>
+            )}
+          </div>
         );
     }
 }
